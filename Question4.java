5346de6e0fd6de12241c5dfb291128bf16a8b9dc
DefinitelyNotAnt <150310592+DefinitelyNotAnt@users.noreply.github.com>
2024-04-15 14:51:51 +0800

    Update 4

--- Question4.java
+++ Question4.java
@@ -19,7 +19,7 @@ public class Question4
      
     Scanner in = new Scanner(System.in);
     double base = in.nextInt();
-    for(int i = 1;i <= base; i++){
+    for(base;base >= 0; base--){
       for(int a = 0; a<=i;a++){
         System.out.print("*");
       }
